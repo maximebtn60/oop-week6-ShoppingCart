29651f931801339c5ffacf9e1155f12490d8c66a
Maxime <maxime.boutin@edu.devinci.fr>
2020-10-19 16:08:21 +0200

    Ajoutez des fichiers projet.

---
+++ ShoppingCard/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCard
+{
+    class Item
+    {
+        public double Price { get; set; }
+        public string Fruit { get; set; }
+        
+        public Item(string fruit)
+        {
+            Fruit = fruit;
+            if (fruit == "apple")
+                Price = 0.25;
+            else Price = 0.60;
+        }
+    }
+}
---
+++ ShoppingCard/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingCard
+{
+    class Program
+    {
+        static double TotalPrice(List<Item> list)
+        {
+            double res = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                res = res + list[i].Price;
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
